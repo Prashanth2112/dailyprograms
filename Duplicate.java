df6b01ebffae79991eadbb131c9f9ad441225e8f
Prashanth2112 <65341802+Prashanth2112@users.noreply.github.com>
2020-05-20 13:12:24 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,16 @@
+package SeleniumWorkout;
+
+import java.util.Arrays;
+
+public class Duplicate {
+	public static void main(String[] args) {
+	int [] arr = {22,12,45,67,45,33,2,12,98,5,7,2};
+	Arrays.sort(arr);
+	
+		for(int i=0;i<arr.length-1;i++)
+		{
+			
+		}
+	}
+
+}
